cf6c8fa57e76b2a5a97c3829b186176588b0619b
Dawid <dawidgorskimail.gmail.com>
2023-02-12 20:48:33 +0100

    Refactoring

--- src/test/java/config/DriverSingleton.java
+++ src/test/java/config/DriverSingleton.java
@@ -3,7 +3,6 @@ package config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public final class DriverSingleton {
     private DriverSingleton() {
@@ -14,8 +13,6 @@ public final class DriverSingleton {
     public static WebDriver getInstance() {
         if (instance == null) {
             WebDriverManager.chromedriver().setup();
-//            ChromeOptions options = new ChromeOptions().setHeadless(true);
-//            instance = new ChromeDriver(options);
             instance = new ChromeDriver();
         }
         return instance;
