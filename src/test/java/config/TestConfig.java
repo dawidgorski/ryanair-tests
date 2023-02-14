9de9d0507eaeabd4638eb338c6a75e1eb51732fd
Dawid <dawidgorskimail.gmail.com>
2023-02-14 12:01:51 +0100

    Add mainPage variable

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -5,30 +5,33 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import pages.MainPage;
 
 import static utility.Actions.waitForClickabilityAndClick;
 
 public class TestConfig {
+
     public static WebDriver driver;
+    public MainPage mainPage;
 
     @BeforeAll
-    public static void setupAll(){
+    public static void setupAll() {
         driver = DriverSingleton.getInstance();
         driver.get("https://www.ryanair.com/us/en");
         driver.manage().window().maximize();
         waitForClickabilityAndClick(By.cssSelector("[data-ref='cookie.accept-all']"));
     }
 
-    @BeforeEach
-    public void setupEach(){
-        driver.get("https://www.ryanair.com/us/en");
-
+    @AfterAll
+    public static void teardownAll() {
+        driver.quit();
+        DriverSingleton.quit();
     }
 
-    @AfterAll
-    public static void teardownAll(){
-    driver.quit();
-    DriverSingleton.quit();
+    @BeforeEach
+    public void setupEach() {
+        driver.get("https://www.ryanair.com/us/en");
+        mainPage = new MainPage();
     }
 
 }
