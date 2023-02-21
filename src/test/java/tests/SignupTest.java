e5f6740c87712374dc4ddf8e4cc80c73c5e322ca
Dawid <dawidgorskimail.gmail.com>
2023-02-21 10:28:44 +0100

    Add csv parameters and csv file to signupWithWrongPasswordAndGetPasswordErrors method

--- src/test/java/tests/SignupTest.java
+++ src/test/java/tests/SignupTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import config.TestConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import pages.SignupWindow;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SignupTest extends TestConfig {
+
 
     @Test
     public void signupCorrectCredentialsAccountExist() {
@@ -46,7 +49,9 @@ public class SignupTest extends TestConfig {
         assertEquals("Password is required", passwordError);
     }
 
-    public void signupWithWrongPasswordAndGetPasswordErrors(String password, boolean one_number_requirement, boolean eight_characters, boolean one_lower, boolean one_upper) {
+    @ParameterizedTest
+    @CsvFileSource(resources = "wrong-password-date.csv")
+    public void signupWithWrongPasswordAndGetPasswordErrors( boolean one_number_requirement, boolean eight_characters, boolean one_lower, boolean one_upper, String password) {
         SignupWindow signupWindow = mainPage
                 .openSignUpWindow()
                 .signUpWithCredentials("test@qa.team", password);
@@ -62,44 +67,5 @@ public class SignupTest extends TestConfig {
         assertEquals(one_upper, passwordErrorsList.get(3), "At least one upper case letter");
     }
 
-    @Test
-    public void signupWithWrongPassword_onlyLowerCase() {
-        signupWithWrongPasswordAndGetPasswordErrors("a", false, false, true, false);
-    }
-
-    @Test
-    public void signupWithWrongPassword_onlyUpperCase() {
-        signupWithWrongPasswordAndGetPasswordErrors("A", false, false, false, true);
-    }
-
-    @Test
-    public void signupWithWrongPassword_onlyNumber() {
-        signupWithWrongPasswordAndGetPasswordErrors("1", true, false, false, false);
-    }
-
-    @Test
-    public void signupWithWrongPassword_OneLowerCaseOneUpper() {
-        signupWithWrongPasswordAndGetPasswordErrors("aA", false, false, true, true);
-    }
-
-    @Test
-    public void signupWithWrongPassword_noNumber() {
-        signupWithWrongPasswordAndGetPasswordErrors("TestTest", false, true, true, true);
-    }
-
-    @Test
-    public void signupWithWrongPassword_noEightCharacters() {
-        signupWithWrongPasswordAndGetPasswordErrors("Test1", true, false, true, true);
-    }
-
-    @Test
-    public void signupWithWrongPassword_noUpperCase() {
-        signupWithWrongPasswordAndGetPasswordErrors("eesteeee1", true, true, true, false);
-    }
-
-    @Test
-    public void signupWithWrongPassword_noLowerCase() {
-        signupWithWrongPasswordAndGetPasswordErrors("EAHAHSHASS1", true, true, false, true);
-    }
 
 }
