9920772a7535ebc6eb582e763f08c97f7195754d
Dawid <dawidgorskimail.gmail.com>
2023-02-25 21:53:04 +0100

    Add screenshot to Allure

--- src/test/java/utility/RyanAirTestListener.java
+++ src/test/java/utility/RyanAirTestListener.java
@@ -1,6 +1,7 @@
 package utility;
 
 import config.DriverSingleton;
+import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -9,11 +10,12 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
 public class RyanAirTestListener implements ITestListener {
-
+    private String path = "screenshots";
     public void onTestFailure(ITestResult result) {
         System.out.println("test failed- " + result.getName());
         try {
@@ -23,10 +25,18 @@ public class RyanAirTestListener implements ITestListener {
         }
     }
 
-    public void takeScreenshot(WebDriver driver, String testMethodName) throws IOException {
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] takeScreenshot(WebDriver driver, String testMethodName) throws IOException {
         Date d = new Date();
-        String TimeStamp = d.toString().replace(":", "_").replace(" ", "_");
-        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(srcFile, new File("./screenshots/" + testMethodName + "_" + TimeStamp + ".png"));
+        String timeStamp = d.toString().replace(":", "_").replace(" ", "_");
+
+        new File(path).mkdirs();
+        FileOutputStream out = new FileOutputStream(path + File.separator + testMethodName + "_" + timeStamp + ".png");
+        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        out.write(screenshot);
+        return screenshot;
+
+
     }
+
 }
