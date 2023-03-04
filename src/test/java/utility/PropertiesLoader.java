2f7b6c2831013d8f6398972a480fb9b14af26f8c
Dawid <dawidgorskimail.gmail.com>
2023-03-04 13:42:51 +0100

    Change config location

--- src/test/java/utility/PropertiesLoader.java
+++ src/test/java/utility/PropertiesLoader.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 public class PropertiesLoader {
     public static String loadProperty(String propertyName) throws IOException {
-        InputStream inputStream = new FileInputStream("src/test/resources/config.properties");
+        InputStream inputStream = new FileInputStream("src/test/resources/properties/config.properties");
         Properties properties = new Properties();
         properties.load(inputStream);
         return properties.getProperty(propertyName);
